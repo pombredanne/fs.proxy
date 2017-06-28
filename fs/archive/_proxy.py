ff095350c9db85a2dd672b5a8de95601c4d27d9c
Martin Larralde <martin.larralde@ens-cachan.fr>
2017-06-28 22:02:05 +0200

    Start implementing a wrap proxy writer using a swap-like mecanism

--- fs/archive/_proxy.py
+++ fs/archive/_proxy.py
@@ -1,6 +1,7 @@
 
 import abc
 import six
+import psutil
 import itertools
 import functools
 
@@ -9,7 +10,7 @@ from .. import iotools
 
 from ..base import FS
 from ..mode import Mode
-from ..copy import copy_file, copy_dir
+from ..copy import copy_file, copy_dir, copy_fs
 from ..path import dirname, relpath, join
 from ..tools import get_intermediate_dirs
 from ..opener import open_fs
@@ -21,7 +22,13 @@ from . import _utils
 
 
 class WrapProxyWriterMeta(abc.ABCMeta):
-    """Prevent the wrapped class from using ``WrapFS`` method implementations.
+    """Prevent the wrapped class from using `WrapFS` implementations.
+
+    With this metaclass, the `WrapProxyWriter` methods use any other
+    available implementation , falling back to `WrapFS` in last resort.
+    This prevents the wrapper from directly using the delegate filesystem
+    methods, and instead forces it to use `FS` implementations that rely
+    on the *essential* methods, which have been implemented.
     """
 
     def __new__(cls, name, bases, attrs):
@@ -37,17 +44,27 @@ class WrapProxyWriterMeta(abc.ABCMeta):
 
 @six.add_metaclass(WrapProxyWriterMeta)
 class WrapProxyWriter(WrapFS):
-    """Makes a read-only Filesystem writable.
+    """A wrapper that makes a read-only FS writable with a proxy filesystem.
 
     Uses a temporary filesystem to handle the changes within the
     read-only filesystem, without copying the whole read-only filesystem
     to the temporary one.
+
     """
 
     def __init__(self, wrap_fs=None, proxy=None, close=True):
+        """Create a new WrapProxyWriter instance.
 
+        Parameters:
+            wrap_fs (FS): The read_only filesystem to wrap. If None given,
+                the wrapper will behave exactly as its proxy filesystem alone.
+            proxy (FS): The proxy filesystem. If None given, uses a
+                `TempFS` instance. *Must be writable.*
+            close (bool): Close the read_only filesystem when the wrapper
+                is closed.
+        """
         super(WrapProxyWriter, self).__init__(wrap_fs or MemoryFS())
-        self._proxy = open_fs(proxy or 'temp://__proxy__')
+        self._proxy = open_fs(proxy or 'temp://__proxy__', writeable=True)
         self._removed = set()
         self._close_ro = close
 
@@ -70,6 +87,10 @@ class WrapProxyWriter(WrapFS):
 
     def _on_wrapped_only(self, path):
         """Return True if given ``path`` is present only on the wrapped FS.
+
+        The path is considered present only on the wrapped filesystem if:
+            * it has never been modified
+            * it has not been removed
         """
         return self.delegate_fs().exists(path) \
            and not self._proxy.exists(path) \
@@ -77,6 +98,12 @@ class WrapProxyWriter(WrapFS):
 
     def _relocate(self, path):
         """Move the entry at the given ``path`` to the proxy FS.
+
+        Note:
+            Uses `fs.copy.copy_file`, although since the copy is
+            always done across different filesystems, there is limited
+            chances the copy will be more optimized than reading/writing
+            two file objects.
         """
         self._proxy.makedirs(dirname(path), recreate=True)
         copy_file(self.delegate_fs(), path, self._proxy, path)
@@ -202,3 +229,62 @@ class WrapProxyWriter(WrapFS):
     def validatepath(self, path):
         self.check()
         return super(WrapFS, self).validatepath(path)
+
+
+class WrapSwapProxyWriter(WrapProxyWriter):
+    """Similar to `WrapProxyWriter`, but with a dynamic proxy.
+
+    The wrapper starts with a MemoryFS, but if the available memory
+    on the system becomes low, it will swap automatically to a
+    TempFS to free some space.
+
+    Attributes:
+        MEMORY_USAGE_LIMIT (int): the maximum size of the RAM proxy.
+            If the memory filesystem occupies more than that, the
+            wrapper swaps to the backup filesystem. Defaults to half
+            of the total memory the system.
+        memory_usage (int): the cumulated size of all files in the
+            proxy filesystem.
+
+    Todo:
+        Make the swap dynamic, so that the wrapper bounces between
+        the intial proxy and the swapped proxy instead of always
+        using the swapped proxy after the swap.
+
+        Fix the _swap causing things to crash.
+
+    """
+    MEMORY_USAGE_LIMIT = psutil.virtual_memory().total / 2
+
+    def __init__(self, wrap_fs=None, swap_fs=None, close=True):
+        super(WrapSwapProxyWriter, self).__init__(
+            wrap_fs or MemoryFS(),
+            proxy=MemoryFS()
+        )
+        self._swap_fs = open_fs(proxy or 'temp://__swap__', writeable=True)
+
+    @property
+    def memory_usage(self):
+        """The sum of the proxy filesystem files sizes.
+        """
+        memory_usage = 0
+        for _, info in self._proxy.walk.info(namespaces=("details")):
+            memory_usage += info.size or 0
+        return memory_usage
+
+    def _relocate(self, path):
+        super(WrapSwapProxyWriter, self)._relocate(path)
+
+        if self.memory_usage > self.MEMORY_USAGE_LIMIT:
+            self._swap()
+
+    def _swap(self):
+        """Replace the current proxy with the backup proxy.
+
+        The in-memory proxy filesystem is closed after the copy.
+        """
+        if self._proxy is not self._swap_fs:
+            _proxy, self._proxy = self._proxy, self._swap_fs
+            copy_fs(_proxy, self._proxy)
+            _proxy.close()
+            del _proxy
