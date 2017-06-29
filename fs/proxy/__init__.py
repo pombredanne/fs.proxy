9207e624afe8947f6caa5b65862442fd570b7efb
Martin Larralde <martin.larralde@ens-cachan.fr>
2017-06-29 10:58:06 +0200

    Add module docstring

--- fs/proxy/__init__.py
+++ fs/proxy/__init__.py
@@ -1,20 +1,28 @@
 # coding: utf-8
+"""A collection of wrapper filesystems.
 
-import pkg_resources
-import email
+These classes inherit from `fs.wrapfs.WrapFS`, and allow wrapping a
+filesystem without relying only the delegate filesystem method
+implementations. In other terms, filesystem inheriting from `fs.proxy.Proxy`
+will have access to the ``delegate_fs`` of `fs.wrapfs.WrapFS`, but other
+than that will behave like `fs.base.FS`. Implementing a `fs.proxy.Proxy`
+is only as tough as implementing a plain filesystem.
+"""
 
+# Copyright (c) 2017 Martin Larralde
+# coding: utf-8
+
+__license__ = "MIT"
+__copyright__ = "Copyright (c) 2017 Martin Larralde"
+__author__ = "Martin Larralde"
+
+# Dynamically get the version
 try:
-    dist = pkg_resources.get_distribution(__name__)
-    info = email.message_from_string(dist.get_metadata(dist.PKG_INFO))
+    import pkg_resources
+    __version__ = pkg_resources.get_distribution(__name__).version
 except Exception:
-    dist = None
-    info = None
+    pkg_resources
 else:
-    __version__ = dist.version
-    __author__ = '{} <{}>'.format(info['Author'], info['Author-email'])
-    __license__ = 'Copyright (c) 2017 {}'.format(info['Author'])
+    __version__ = 'dev'
 finally:
-    del dist
-    del info
-    del email
     del pkg_resources
