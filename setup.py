0f28fe81d7582d0810b4fcf695b92a216e046c05
Martin Larralde <martin.larralde@ens-cachan.fr>
2017-06-29 14:46:32 +0200

    Move to nosetests for test runs

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
 
 import setuptools
-setuptools.setup()
+setuptools.setup(
+    setup_requires=["nose"],    # workaround until setup.cfg
+)                               # uses setup_requires for real
