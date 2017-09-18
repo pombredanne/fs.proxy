055296ef00505decca86e8d26a38e9f467ed1781
Martin Larralde <martin.larralde@ens-cachan.fr>
2017-09-18 15:04:33 +0200

    Switch to green for running tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,7 +1,14 @@
+# coding: utf-8
 """Tests for fs.proxy.
 """
-
+from __future__ import unicode_literals
 from __future__ import absolute_import
 
-from . import test_proxy_writer
-from . import test_utils
+import fs
+import os
+import pkg_resources
+
+
+# Add the local code directory to the `fs` module path
+fs.__path__.insert(0, os.path.realpath(
+    os.path.join(__file__, '..', '..', 'fs')))
